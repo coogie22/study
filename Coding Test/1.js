ae4c85f903b0ee0280f0102720641d2c0bb1a197
leejaewon <leejaewon6463@icloud.com>
2024-10-01 01:03:39 +0900

    2024-10-01  1:03:29.79

--- Coding Test/1.js
+++ Coding Test/1.js
@@ -48,6 +48,5 @@ function solution(quiz) {
 }
 
 
-
 console.log(solution(["3 - 4 = -3", "5 + 6 = 11"]));  // ["X", "O"]
 console.log(solution(["19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"]));  // ["O", "O", "X", "O"]
